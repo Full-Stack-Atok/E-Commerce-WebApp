66a4f872e30c099ae854d3ee8bcb18015bf929b8
Full-Stack-Atok <komsaykerby@gmail.com>
2025-04-11 21:56:02 +0800

    Product Routes (CRUD) PART 1

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 
 // routes
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 
+// Environment variables
 dotenv.config();
-import {connectDB} from "./lib/db.js"
+
+// Connection to the database
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
-  connectDB()
+  connectDB();
 });
