1f071ff465ef7caf8f43bbea7b337c3017ed99d7
Full-Stack-Atok <komsaykerby@gmail.com>
2025-04-13 11:15:29 +0000

    Payment Routes with Stripe

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -42,4 +42,4 @@ export const adminRoute = (req, res, next) => {
   } else {
     return res.status(403).json({ message: "Access denied - Admin only" });
   }
-}
+};
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ import cookieParser from "cookie-parser";
 // routes
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 
 // Environment variables
 dotenv.config();
@@ -18,9 +21,12 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
-
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
