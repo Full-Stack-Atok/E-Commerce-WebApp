fb6aa83bd12e230562f0aace2236c05725ca3c65
Full-Stack-Atok <komsaykerby@gmail.com>
2025-05-04 22:05:52 +0800

    2

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -14,6 +14,8 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/refresh-token", refreshToken);
 router.post("/logout", logout);
+
+// This GET returns the user JSON, using protectRoute
 router.get("/profile", protectRoute, getProfile);
 
 export default router;
