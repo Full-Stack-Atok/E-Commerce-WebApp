6e44f205a3f642ebb4ba947de0bd025be453058d
Full-Stack-Atok <komsaykerby@gmail.com>
2025-05-05 09:58:25 +0800

    1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom"; // ✅ use BrowserRouter
 import App from "./App.jsx";
-
-import { HashRouter } from "react-router-dom";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* HashRouter ensures all routes load index.html on reload */}
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
