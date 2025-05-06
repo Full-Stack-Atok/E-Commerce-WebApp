0430f743c76704321d86a85827de8dd79defbd99
Full-Stack-Atok <komsaykerby@gmail.com>
2025-05-06 20:45:17 +0800

    FORGOT TO REMOVE

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,20 +1,17 @@
+// backend/routes/payment.route.js
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import {
   createCheckoutSession,
   checkoutSuccess,
-  handleGCashCallback,
 } from "../controllers/payment.controller.js";
 
 const router = express.Router();
 
-// Card, COD & GCash all go through one endpoint:
+// Card, PayPal & Cash-on-Delivery all go through one endpoint:
 router.post("/create-checkout-session", protectRoute, createCheckoutSession);
 
 // Stripe success callback (or frontend will POST here)
 router.post("/checkout-success", protectRoute, checkoutSuccess);
-
-// Xendit webhook (no auth)
-router.post("/gcash/callback", express.json(), handleGCashCallback);
 
 export default router;
